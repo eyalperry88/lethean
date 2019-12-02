a565ba9d2ea01efb3c77c0b8923753ef6e0c2973
Eyal Perry <eyal.perry88@gmail.com>
2019-12-02 15:27:50 -0500

    fixed conv1 in resnet

--- utils/train_helpers.py
+++ utils/train_helpers.py
@@ -11,8 +11,11 @@ import torch.utils.data
 
 from utils.rotation import RotateImageFolder
 
-normalize = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-tr_transforms = transforms.Compose([transforms.ToTensor(),
+#normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+#normalize = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+tr_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+									transforms.ToTensor(),
 									normalize])
 te_transforms = transforms.Compose([transforms.ToTensor(),
 									normalize])
