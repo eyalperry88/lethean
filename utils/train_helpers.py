030c1c69ba2e96db902ac98a9f5d3c21875050a2
Eyal Perry <eyal.perry88@gmail.com>
2019-12-03 13:01:14 -0500

    decrease lr every 50 epochs instead of 30

--- utils/train_helpers.py
+++ utils/train_helpers.py
@@ -120,7 +120,7 @@ def prepare_test_data(args, use_transforms=True, shuffle=False):
 
 def adjust_learning_rate(optimizer, epoch, args):
 	"""Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-	lr = args.lr * (0.1 ** (epoch // 30))
+	lr = args.lr * (0.1 ** (epoch // 50))
 	for param_group in optimizer.param_groups:
 		param_group['lr'] = lr
 
