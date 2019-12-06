7d97aa4bdbfd74c8725973d2c0e9b3dd4571f7e5
Eyal Perry <eyal.perry88@gmail.com>
2019-12-06 01:07:37 -0500

    .

--- adversarial_fgsm.py
+++ adversarial_fgsm.py
@@ -95,6 +95,7 @@ for i in range(args.epochs):
     data_grad = img.grad.data
 
     # Call FGSM Attack
+    print(img.size())
     perturbed_img = fgsm_attack(img, args.epsilon, data_grad)
     adapt_single_tensor(net, perturbed_img, optimizer, criterion, args.niter, args.batch_size)
 
